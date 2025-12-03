ba3b8d11a35727b7f5b10ecad02dd0beab7140a3
Ikonnikov Bogdan <krefature@mail.com>
2025-12-03 16:22:12 +0600

    first commit

---
+++ src/main/java/krefature/studvisit/StudVisitApplication.java
@@ -0,0 +1,13 @@
+package krefature.studvisit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudVisitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudVisitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/krefature/studvisit/StudVisitApplicationTests.java
@@ -0,0 +1,13 @@
+package krefature.studvisit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudVisitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
